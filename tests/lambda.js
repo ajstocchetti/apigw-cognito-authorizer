ac6a08dd507329b606a8b5bcfd94eaa96d4d2cc6
Andy Stocchetti <astocchetti@servercentral.com>
2020-04-26 20:06:00 -0500

    some tests, but could use more

--- tests/lambda.js
+++
@@ -1,8 +0,0 @@
-const cognitoAuthorizer = require('../index');
-
-const auth = new cognitoAuthorizer({
-  userPoolId: process.env.USER_POOL_ID,
-  region: process.env.REGION,
-});
-
-exports.handler = async(event, context) => await auth.processEvent(event);
